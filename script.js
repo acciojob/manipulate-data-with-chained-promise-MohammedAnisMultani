b77783270e66281256621863e7bb33b053bd99c0
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-04-02 13:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+let output = document.getElementById("output")
+
+let arr = [1,2,3,4]
+function array(data){
+return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(data)
+    },1000)
+
+})
+}
+function array1(data){
+return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve(data)
+    },3000)
+
+})
+}
+
+let p1 = array(arr)
+
+p1.then((data)=>{
+    let div = document.createElement("div")
+        let ans = data.filter((value)=>{value%2==0})
+        div.innerText = ans
+        output.append(div)
+        return array1(ans)
+})
+.then((data)=>{
+    let div = document.createElement("div")
+    let ans2 = data.map(element => element*2);
+    div.innerText = ans2
+    output.append(div)
+})
+.catch((err)=>{console.log(err)})
+
